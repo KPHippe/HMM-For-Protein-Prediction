102038728b2ea9bfb69d2f88959bbe0f870fe1c3
Kyle Hippe <kyle.hippe@gmail.com>
2020-01-28 13:41:19 -0800

    updated trainmodels

--- trainModels.py
+++ trainModels.py
@@ -4,8 +4,11 @@ from hmmlearn import hmm
 import pickle
 from tqdm import tqdm
 import numpy as np
-
-
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from multiprocessing import Pool
+import shutil
+#to time the different methods
+import timeit
 def trainModels():
 
     try:
@@ -27,13 +30,107 @@ def trainModels():
     
     
     #train models here
-    i = 0
+    #i = 0
+    print("Training with no optimization")
     for filename in tqdm(trainingDataList):
         HMMTrain(filename)
-        if i > 3:
-            sys.exit()
-        i+=1
+        #if i > 100:
+        #    sys.exit()
+        #i+=1
 
+'''
+This is the multiprocessed version of tranModels
+'''
+def trainModelsProcessPool():
+    #leave one full core for other things 
+    numCPUs = os.cpu_count() - 2
+    try:
+        trainingDataList = os.listdir(os.pardir + '/TrainingDataForHMM')
+    except:
+        print("Training data not present, please run parse data and convert data to create data")
+        sys.exit(0)
+
+
+    '''
+    # Making new folder for models
+    '''
+    try:
+        os.mkdir(os.pardir + "/HMMModels/")
+    except:
+        print("HMMModels folder already made")
+    
+    
+    #train models concurrently here
+    print("Training with ProcessPoolExecutor...") 
+    with ProcessPoolExecutor(max_workers=numCPUs) as executor:
+        executor.map(HMMTrain, trainingDataList)
+
+        '''Old stuff to try to get tqdm to work'''
+        #futures = [executor.submit(HMMTrain, filename) for filename in trainingDataList] 
+        
+        #kwargs = {
+        #    'total': len(futures),
+        #    'unit': 'it',
+        #    'unit_scale': True,
+        #    'leave': True
+        #}
+
+        #tqdm stuff here
+        #for f in tqdm(as_completed(futures), **kwargs):
+        #    pbar.update(1)    
+
+       
+'''
+This is the multiprocessed version of tranModels
+'''
+def trainModelsMultiprocessing():
+    '''Setup multiprocessing'''
+    #leave one full core for other things 
+    numCPUs = os.cpu_count() - 2
+    p = Pool(numCPUs)
+
+    try:
+        trainingDataList = os.listdir(os.pardir + '/TrainingDataForHMM')
+    except:
+        print("Training data not present, please run parse data and convert data to create data")
+        sys.exit(0)
+    '''
+    ##########################################
+    REMOVE THIS LATER
+    ##########################################
+    '''
+    try:
+        testingDataList = os.listdir(os.pardir + "/TestDataForHMM")
+    except: 
+        print("Testing data not present")
+        sys.exit()
+
+    '''
+    # Making new folder for models
+    '''
+    try:
+        os.mkdir(os.pardir + "/HMMModels/")
+    except:
+        print("HMMModels folder already made")
+    
+    
+    '''train models concurrently here'''
+    '''
+    Example: 
+    list(tqdm.tqdm(pool.imap_unordered(do_work, range(num_tasks)), total=len(values)))
+    ''' 
+    #pbar = tqdm(total=len(trainingDataList))
+    #execute trianing here
+    #p.map(HMMTrain, trainingDataList) #works but not quite what I want
+    print("Training using multiprocessing...")
+    #MAKE SURE TO CHANGE BACK TO TRAININGDATA WHEN DONE TESTING
+    list(tqdm(p.imap_unordered(HMMTrain, testingDataList),
+        total=len(testingDataList)))
+
+    p.close()
+    p.join()
+    pbar.close() 
+    
 
 def HMMTrain(filename):
     #parse the data
@@ -41,7 +138,8 @@ def HMMTrain(filename):
     model in '1-2-3-...-27' form
 
     '''
-    with open(os.pardir + "/TrainingDataForHMM/" + filename, "r") as f:
+    #MAKE SURE TO CHANGE WHEN DONE TESTING
+    with open(os.pardir + "/TestDataForHMM/" + filename, "r") as f:
         sequences = f.read().split('\n')
     
     dataForHMM = np.array([]) 
@@ -61,24 +159,29 @@ def HMMTrain(filename):
 
     #train the model
 
-    model = hmm.GaussianHMM(n_components=1, covariance_type='full', n_iter=1000)
+    model = hmm.GaussianHMM(n_components=10, covariance_type='full', n_iter=1000)
 
     model.fit(dataForHMM, lengths)
-    randTest = np.random.rand(dataForHMM.shape[0], dataForHMM.shape[1])
-    print("Model: {} dataForHMM score: {} randomData score: {}".format(filename,
-        np.expm1(model.score(dataForHMM)),np.expm1(model.score(randTest))))
+    #randTest = np.random.rand(dataForHMM.shape[0], dataForHMM.shape[1])
+    #print("dataForHMM score: {} randomData score: {}".format(model.score(dataForHMM),model.score(randTest)))
     
     #pickle the model 
     
     modelName = filename.split('.')[0] + ".mdl"
     with open(os.pardir + "/HMMModels/" + modelName, 'wb') as f:
         pickle.dump(model, f)
-
-
+    
 
 
 if __name__ == "__main__":
-    trainModels()
 
+    try: 
+        shutil.rmtree(os.pardir + "/HMMModels/")
+    except: 
+        pass
+    
+    #multiprocessing is the way to go here, it has tqdm and is just as fast
 
-
+    trainModelsMultiprocessing()
+    
+    
