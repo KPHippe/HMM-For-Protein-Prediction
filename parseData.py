df004314a19dd453c04af0f1ee505a442e58caa0
Kyle Hippe <kyle.hippe@gmail.com>
2020-01-31 21:44:54 -0800

    changed some files to allow for debug on Cloud Compute

--- parseData.py
+++ parseData.py
@@ -160,7 +160,8 @@ def multiProcessingAugmentation(term, sequences):
         training_list_100 = augmentData.augmentData(sequences, 100)
         #training_list_100 = sequences
         result = (term, training_list_100)
-    print(f"result is: {','.join(str(i) for i in result)}")
+    print("returning result from multiprocessingAugmentation in parseData")
+    #print(f"result is: {','.join(str(i) for i in result)}")
     return result
 
 
